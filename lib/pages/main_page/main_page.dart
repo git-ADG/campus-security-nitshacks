4f432a8a2ccbdbe3987695cf1712a910764473de
git-ADG <adg8503@gmail.com>
2024-02-02 11:53:03 +0530

    main page created

---
+++ lib/pages/main_page/main_page.dart
@@ -0,0 +1,86 @@
+import 'package:campus_security_nithacks/pages/pages.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../../utils/utils.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  static const String routeName = '/MainPage';
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List pages = [
+    const SplashScreen(),
+    const SplashScreen(),
+    const SplashScreen(),
+    const SplashScreen(),
+  ];
+  int currentIndex = 1;
+
+  void navigate(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) async {
+        if(currentIndex != 1){
+          navigate(1);
+          return;
+        }
+        if (didPop) {
+          return;
+        }
+        final bool shouldPop = await showExitWarning(context);
+        if (shouldPop) {
+          SystemNavigator.pop();
+        }
+      },
+      child: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.red,
+          type: BottomNavigationBarType.fixed,
+          onTap: navigate,
+          currentIndex: currentIndex,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(
+                CupertinoIcons.profile_circled,
+              ),
+              label: "Account",
+            ),
+            BottomNavigationBarItem(
+              label: "Home",
+              icon: Icon(
+                Icons.home,
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: "Cart",
+              icon: Icon(
+                CupertinoIcons.cart,
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: "Notifications",
+              icon: Icon(
+                CupertinoIcons.bell,
+              ),
+            ),
+          ],
+        ),
+        body: pages[currentIndex],
+      ),
+    );
+  }
+}

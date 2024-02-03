3af071e2419e2931f89bb2ddccc85b3c86e2fa9c
git-ADG <adg8503@gmail.com>
2024-02-03 12:51:41 +0530

    image picker added

--- lib/pages/alert_page/alert_page.dart
+++ lib/pages/alert_page/alert_page.dart
@@ -1,6 +1,7 @@
 import 'package:campus_security_nithacks/config/utils/palette.dart';
 import 'package:campus_security_nithacks/pages/warning_pages/alarm_page.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AlertPage extends StatefulWidget {
   const AlertPage({super.key});
@@ -10,6 +11,15 @@ class AlertPage extends StatefulWidget {
 }
 
 class _AlertPageState extends State<AlertPage> {
+
+  Future<void> _getImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      print("Image picked: ${pickedFile.path}");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -24,14 +34,14 @@ class _AlertPageState extends State<AlertPage> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                SizedBox(
+                const SizedBox(
                   height: 120,
                 ),
-                Text(
+                const Text(
                   "See something suspicious?",
                   style: TextStyle(color: Colors.white, fontSize: 20),
                 ),
-                Row(
+                const Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
@@ -51,70 +61,70 @@ class _AlertPageState extends State<AlertPage> {
                   width: width * 0.8,
                   height: height * 0.6,
                   decoration: BoxDecoration(
-                    color: Color.fromRGBO(30, 30, 30, 1),
+                    color: const Color.fromRGBO(30, 30, 30, 1),
                     borderRadius: BorderRadius.circular(10),
                   ),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text(
+                      const Text(
                         "Issue(title)",
                         style: TextStyle(color: Colors.white),
                       ),
                       TextFormField(
-                        decoration: InputDecoration(
+                        decoration: const InputDecoration(
                             fillColor: Colors.grey,
                             border: OutlineInputBorder(
                                 borderSide: BorderSide(width: 5))),
                       ),
-                      SizedBox(
+                      const SizedBox(
                         height: 10,
                       ),
-                      Text(
+                      const Text(
                         "Location",
                         style: TextStyle(color: Colors.white),
                       ),
                       TextFormField(
-                        decoration: InputDecoration(
+                        decoration: const InputDecoration(
                             fillColor: Colors.grey,
                             border: OutlineInputBorder(
                                 borderSide: BorderSide(width: 5))),
                       ),
-                      SizedBox(
+                      const SizedBox(
                         height: 10,
                       ),
-                      Text(
+                      const Text(
                         "Scholar ID",
                         style: TextStyle(color: Colors.white),
                       ),
                       TextFormField(
-                        decoration: InputDecoration(
+                        decoration: const InputDecoration(
                             fillColor: Colors.grey,
                             border: OutlineInputBorder(
                                 borderSide: BorderSide(width: 5))),
                       ),
-                      SizedBox(
+                      const SizedBox(
                         height: 20,
                       ),
                       IconButton(
-                          onPressed: () {},
-                          icon: Icon(
+                          onPressed: _getImage,
+                          icon: const Icon(
                             Icons.camera_alt_outlined,
                             size: 100,
                           )),
-                      Text(
+                      const Text(
                         "Add Picture(optional)",
                         style: TextStyle(color: Colors.white),
                       ),
                       ElevatedButton(
-                          style: ButtonStyle(
+                          style: const ButtonStyle(
                               backgroundColor: MaterialStatePropertyAll(red)),
                           onPressed: () => Navigator.push(
                               context,
                               MaterialPageRoute(
-                                builder: (context) => AlarmPage(),
+                                builder: (context) => const AlarmPage(),
                               )),
-                          child: Text(
+                          child: const Text(
                             "Report",
                             style: TextStyle(color: Colors.white),
                           ))

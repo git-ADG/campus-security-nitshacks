8eb19aaf74011700fedda2e06aba6a778ba5dc98
Diptanghsu-DC <115690935+Diptanghsu-DC@users.noreply.github.com>
2024-02-04 01:34:36 +0530

    added go_back route

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -3,3 +3,4 @@ export 'package:campus_security_nithacks/utils/formatter.dart';
 export 'package:campus_security_nithacks/utils/logout_message.dart';
 export 'package:campus_security_nithacks/utils/screen_size.dart';
 export 'package:campus_security_nithacks/utils/toast_message.dart';
+export 'package:campus_security_nithacks/utils/go_back.dart';

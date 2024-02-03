fe1d876d9fbb320fdae14eb1f8636662a5401c7b
Gaurav-822 <gaurav.moocs@gmail.com>
2024-02-03 12:02:53 +0530

    Update alarm_page.dart

--- lib/pages/warning_pages/alarm_page.dart
+++ lib/pages/warning_pages/alarm_page.dart
@@ -28,37 +28,40 @@ class _AlarmPageState extends State<AlarmPage>
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 48),
-            child: ScaleTransition(
-              scale: Tween<double>(begin: 1, end: 1.1).animate(_controller),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Image.asset(
-                  'assets/images/warning.jpg',
-                  width: 300,
-                  height: 200,
-                  fit: BoxFit.cover,
+    return Container(
+      color: Colors.black, // Set the background color to black
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 48),
+              child: ScaleTransition(
+                scale: Tween<double>(begin: 1, end: 1.1).animate(_controller),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.asset(
+                    'assets/images/warning.jpg',
+                    width: 300,
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 20),
-          const Text(
-            'Sending Alert \n Stay Calm',
-            style: TextStyle(
-              fontFamily: 'Outfit',
-              color: Colors.white,
-              fontSize: 32,
+            const SizedBox(height: 20),
+            const Text(
+              'Sending Alert \n Stay Calm',
+              style: TextStyle(
+                fontFamily: 'Outfit',
+                color: Colors.white,
+                fontSize: 32,
+              ),
+              textAlign: TextAlign.center,
             ),
-            textAlign: TextAlign.center,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

2848b6dd4255c3699926fd2a6fd7da0761d075b5
Diptanghsu-DC <115690935+Diptanghsu-DC@users.noreply.github.com>
2024-02-04 01:26:09 +0530

    added routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:campus_security_nithacks/pages/alert_page/alert_page.dart';
+import 'package:campus_security_nithacks/pages/login_page/login_page.dart';
+import 'package:campus_security_nithacks/pages/signup_page/signup_page.dart';
+import 'package:campus_security_nithacks/pages/warning_pages/alarm_page.dart';
 import 'package:flutter/material.dart';
 import 'pages/pages.dart';
-
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,8 +15,18 @@ class CampusSecurity extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: FingerprintPage(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        FingerprintPage.routeName: (context) => const FingerprintPage(),
+        LoginPage.routeName: (context) => const LoginPage(),
+        SIgnUpPage.routeName: (context) => const SIgnUpPage(),
+        SplashScreen.routeName: (context) => const SplashScreen(),
+        AlertPage.routeName: (context) => const AlertPage(),
+        AlarmPage.routeName: (context) => const AlarmPage(),
+        MapPage.routeName: (context) => const MapPage(),
+      },
+      initialRoute: FingerprintPage.routeName,
     );
   }
 }
